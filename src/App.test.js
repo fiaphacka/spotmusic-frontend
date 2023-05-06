06977de78e0e6b4f66cf5f3780e53129ff6f3cdb
viniciushrcs <viniciushrcs@gmail.com>
2023-05-06 10:54:20 -0300

    test: app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('verifica se os elementos da tabela estão sendo renderizados', async () => {
+  const fakeData = [
+    {
+      song_id: 1,
+      title: "Música Exemplo",
+      artist: "Artista Exemplo",
+      genre: "Gênero Exemplo",
+    },
+  ];
+
+  jest.spyOn(global, "fetch").mockImplementation(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(fakeData),
+      })
+  );
+
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const songIdElement = await screen.findByText("1");
+  const titleElement = screen.getByText(/Música Exemplo/i);
+  const artistElement = screen.getByText(/Artista Exemplo/i);
+  const genreElement = screen.getByText(/Gênero Exemplo/i);
+
+  expect(songIdElement).toBeInTheDocument();
+  expect(titleElement).toBeInTheDocument();
+  expect(artistElement).toBeInTheDocument();
+  expect(genreElement).toBeInTheDocument();
+
+  global.fetch.mockRestore();
 });
